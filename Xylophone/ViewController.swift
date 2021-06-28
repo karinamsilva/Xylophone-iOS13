a55ae5873339324179773a746adcd34780e17d11
vpn <vpn@NBBV027658.local>
2021-06-27 21:26:54 -0300

    changes added

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,40 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
     
+    var player: AVAudioPlayer!
 
+
+    @IBAction func keyPressed(_ sender: UIButton) {
+        guard let send = sender.titleLabel?.text else {return}
+        print(send)
+        playSound(sound: send)
+        sender.alpha = 0.5
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1.0
+        }
+        
+        
+    }
+    
+    
+    
+    func playSound(sound: String) {
+        let url = Bundle.main.url(forResource: sound, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
+
+
+
 
